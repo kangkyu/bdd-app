1cec9542b6c13b73de152e74659f370a54edfda5
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-08-18 18:06:09 -0700

    Add before_action :authenticate_user! callback to ApplicationController
    And pass
    rspec spec/controllers/home_controller_spec.rb

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
+    let(:user) { instance_double(User) }
+
     it "returns http success" do
+      log_in(user)
+
       get :index
-      expect(response).to have_http_status(:success)
+      # expect(response).to have_http_status(:success)
+      expect(response.status).to be(200)
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require "support/controller_helpers"
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -54,6 +56,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Warden::Test::Helpers
+  # config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers
+  config.include ControllerHelpers, :type => :controller
 end
 
 Shoulda::Matchers.configure do |config|
